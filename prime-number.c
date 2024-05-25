da51b608f03f58145ad0d7cb9f2c0f5ce96e82df
nicklamiller <nicklamiller@gmail.com>
2024-05-24 18:19:35 -0700

    basic c scripts

---
+++ prime-number.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+
+void checkIsPrime(int num) {
+    int i;
+    for (i = 2; i <= num / 2; i++) {
+        if (num % i == 0) {
+            printf("The %d is not prime number.\n", num);
+            return;
+        }
+    }
+    printf("The %d is a prime number.\n", num);
+    return;
+}
+
+
+int main() {
+    int num;
+    printf("Input a number: ");
+    scanf("%d", &num);
+    checkIsPrime(num);
+    return 0;
+}
---
+++ print-integer.c
@@ -0,0 +1,12 @@
+# include <stdio.h>
+
+
+int main() {
+    int num;
+    printf("Please enter a number: ");
+    scanf("%d", &num);
+
+    printf("Your number is: %d\n", num);
+    return 0;
+}
+
