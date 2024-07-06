d985341689851de9473d69f3fb543efee782ad48
nicklamiller <nicklamiller@gmail.com>
2024-07-05 21:38:37 -0700

    control flow exercise

---
+++ control_flow/multiplication_table.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+
+void print_table(int num, int range) {
+    for (int i; i <= range; i++) {
+        printf("%d * %d = %d\n", num, i, num * i);
+    }
+}
+
+
+int main() {
+    int num = 5;
+    int range = 10;
+    print_table(num, range);
+    return 0;
+}
