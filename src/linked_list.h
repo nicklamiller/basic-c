a1bca30147a6543959d27e0a480dd1c605567470
Nick Miller <58093869+nicklamiller@users.noreply.github.com>
2025-11-25 11:37:28 -0800

    Add linked_list header file plus initial unit test (#3)

---
+++ src/linked_list.h
@@ -0,0 +1,14 @@
+#pragma once
+
+
+typedef struct Node {
+    int data;
+    struct Node* next;
+} Node;
+
+void print_node(Node* node);
+void print_linked_list(Node* current);
+Node* insert_head(Node* head, int value);
+Node* get_value(Node* current, int idx);
+Node* set_value(Node* current, int idx, int data);
+void destroy_linked_list(Node* head);
